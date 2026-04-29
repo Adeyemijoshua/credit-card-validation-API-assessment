46182f7284fe5b12cdfbf617ab484b3b1a50093d
unknown <adeyemijoshua9999@gmail.com>
2026-04-29 01:24:36 +0100

    fix(config): fix jest config issue

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,14 @@
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.ts'],
-  globals: {
-    'ts-jest': {
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
       tsconfig: {
+        strict: true,
+        esModuleInterop: true,
+        skipLibCheck: true,
         types: ['jest', 'node']
       }
-    }
+    }]
   }
 };
